d8b622d1c80aa408c336b977dca50f6f74a17c75
Varrium <rolandogarciaram@gmail.com>
2022-06-08 23:15:24 -0700

    2nd version of RPS. Basic structure is completed

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-function playerSelection('selection') {
-    let pSelection = prompt("Welcome. Please write 'rock' 'paper' or 'scissors' to play:",' ')
-    return pSelection
+let playerScore = 0
+let computerScore = 0
+
+function playerPlay() {
+    let playerSelection = prompt("Welcome. Please write 'rock' 'paper' or 'scissors' to play:").toLowerCase()
+    return playerSelection
 }
 
 function computerPlay() {
-    let computerOptions = [ "Paper" , "Rock" , "Scissors"]
+    let computerOptions = [ "paper" , "rock" , "scissors"]
     let random = Math.floor((Math.random() * computerOptions.length));
     let computerSelection = computerOptions[random];
     return computerSelection
 }
 
-computerPlay()
-console.log(computerSelection)
-console.log("Hello")
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerPlay()
+    computerSelection = computerPlay()
+    console.log (playerSelection)
+    console.log (computerSelection)
+    if (playerSelection === computerSelection) {
+        return 
+    }
+
+    if (
+        (playerSelection === "paper" && computerSelection === "rock" ) ||
+        (playerSelection === "scissors" && computerSelection === "paper") || 
+        (playerSelection === "rock" && computerSelection === "scissors")  
+        ) { 
+        return ++playerScore    
+     }
+    if (
+        (playerSelection === "paper" && computerSelection === "scissors") || 
+        (playerSelection === "rock" && computerSelection === "paper") || 
+        (playerSelection === "scissors" && computerSelection === "rock")
+        ) { 
+        return ++computerScore 
+    }
+
+    return
+
+}
+
+
+game()
